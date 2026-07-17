2514b5f7b8c0a0e7478fa013e918fd583c3fd6dc
jose rangel <josemrn2006@gmail.com>
2026-07-17 15:09:40 -0500

    feat(backend): proteger CRUD de categorías con requireAdmin

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { getAll, getById, create, update, remove } from "../controllers/category.controller.js";
-import { verifyToken } from "../middlewares/auth.middleware.js";
+import { verifyToken, requireAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/categories", getAll);
 router.get("/categories/:id", getById);
-router.post("/categories", verifyToken, create);
-router.put("/categories/:id", verifyToken, update);
-router.delete("/categories/:id", verifyToken, remove);
+router.post("/categories", verifyToken, requireAdmin, create);
+router.put("/categories/:id", verifyToken, requireAdmin, update);
+router.delete("/categories/:id", verifyToken, requireAdmin, remove);
 
 export default router;
